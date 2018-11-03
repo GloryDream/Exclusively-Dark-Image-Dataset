a778d8fece16c99e431b4176f0000613b82543be
neogong_Mac <xy_Gong@foxmail.com>
2018-11-03 15:17:52 -0500

    update eval_refine

--- libs/eval_refine.py
+++ libs/eval_refine.py
@@ -5,13 +5,51 @@ import argparse
 from torchvision import transforms
 from tqdm import tqdm
 import torch
-
+import collections
+import numpy as np
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--batch_size', type=int, default=1, help='size of the batches')
 parser.add_argument('--topk', type=int, help='The topk acc')
 opt = parser.parse_args()
 print(opt)
+
+cls2imagenet_idx = {
+			'Bicycle': 444,
+			'Boat': 833,
+			'Bottle': [440, 720, 737, 898, 907],
+			'Bus': 779,
+			'Car': [817, 864],
+			'Cat': [281, 282, 283, 284, 285, 383],
+			'Chair': [423, 559, 765],
+			'Cup': [647, 968],
+			'Dog': list(range(150, 276)),
+			'Motorbike': 670,
+			# 'People': 1000,
+			'Table': [736, 532]
+		}
+
+imagenet_idx2cls = {}
+for item in cls2imagenet_idx:
+	if isinstance(cls2imagenet_idx[item], list):
+		for id in cls2imagenet_idx[item]:
+			imagenet_idx2cls[id] = item
+	else:
+		imagenet_idx2cls[cls2imagenet_idx[item]] = item
+
+indices = list(imagenet_idx2cls.keys())
+oreder_imagenet_idx2cls = collections.OrderedDict(sorted(imagenet_idx2cls.items()))
+
+refined_imagenet_idx2cls = {}
+for idx, (_, v) in enumerate(oreder_imagenet_idx2cls.items()):
+	refined_imagenet_idx2cls[idx] = v
+
+refined_imagenet_cls2idx = {}
+for k, v in refined_imagenet_idx2cls.items():
+	if v not in refined_imagenet_cls2idx:
+		refined_imagenet_cls2idx[v] = [k]
+	else:
+		refined_imagenet_cls2idx[v].append(k)
 
 if __name__ == '__main__':
 	resnet50 = models.resnet50(pretrained=True).cuda()
@@ -35,27 +73,21 @@ if __name__ == '__main__':
 	correct = 0
 	for img, label, _ in tqdm(dataloader):
 		output = resnet50(img.type(torch.cuda.FloatTensor))
-		predicted = torch.argmax(output.data, 1).item()
+		output = output.data.cpu().numpy()[0]
+		refined_output = np.take(output, indices)
 		total += 1
-		#print('label: ', label)
-		#print('predicted: ', predicted)
-		if opt.topk is None:
-			if predicted not in dataset.imagenet_idx2cls:
-				continue
-			if dataset.imagenet_idx2cls[predicted] == label[0]:
-				correct += 1
-		else:
-			_, tok_idx = torch.topk(output.data, k=opt.topk, dim=1)
-			tok_idx = tok_idx.cpu().numpy().tolist()[0]
+		top_idx = list(refined_output.argsort()[-opt.topk:][::-1])
 
-			gd = dataset.cls2imagenet_idx[label[0]]
-			if not isinstance(gd, list):
-				gd = [gd]
-			if list(set(gd)&set(tok_idx)) == []:
-				continue
-			else:
-				correct += 1
-	print('Accuracy of the network on the %d test images: %f %%' % (total,
+		if label[0] == 'People':
+			continue
+		gd = cls2imagenet_idx[label[0]]
+		if not isinstance(gd, list):
+			gd = [gd]
+		if list(set(gd)&set(top_idx)) == []:
+			continue
+		else:
+			correct += 1
+	print('Top %d Accuracy of the network on the %d test images: %f %%' % (opt.topk, total,
 			100 * correct / total))
 
 
