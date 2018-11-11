4b255fb5f6625694c3a923d530b0fa0ccae12742
neogong_Mac <xy_Gong@foxmail.com>
2018-11-10 20:50:55 -0600

    compatible

--- libs/finetune.py
+++ libs/finetune.py
@@ -15,20 +15,22 @@ import torch.utils.data
 import torch.utils.data.distributed
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
+import torchvision.models as models
 from resnet_finetune import resnet50
 
-# model_names = sorted(name for name in models.__dict__
-#     if name.islower() and not name.startswith("__")
-#     and callable(models.__dict__[name]))
+
+model_names = sorted(name for name in models.__dict__
+     if name.islower() and not name.startswith("__")
+     and callable(models.__dict__[name]))
 
 parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
 parser.add_argument('data', metavar='DIR',
                     help='path to dataset')
-# parser.add_argument('--arch', '-a', metavar='ARCH', default='resnet18',
-#                     choices=model_names,
-#                     help='model architecture: ' +
-#                         ' | '.join(model_names) +
-#                         ' (default: resnet18)')
+parser.add_argument('--arch', '-a', metavar='ARCH', default='resnet18',
+                     choices=model_names,
+                     help='model architecture: ' +
+                         ' | '.join(model_names) +
+                         ' (default: resnet18)')
 parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
                     help='number of data loading workers (default: 4)')
 parser.add_argument('--epochs', default=90, type=int, metavar='N',
